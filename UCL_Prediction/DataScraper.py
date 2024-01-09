6d767fc2f67104144164ebe9eaaed92b8996026a
katarzynamichalskaa <kate.michalska7@gmail.com>
2024-01-09 21:32:25 +0100

    organizing functions and files

--- UCL_Prediction/DataScraper.py
+++ UCL_Prediction/DataScraper.py
@@ -63,10 +63,3 @@ class DataScraper:
         file_path = f'SEASONS20052023.csv'
         df.to_csv(file_path, mode='a', header=not os.path.exists(file_path), index=False)
 
-
-years = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-
-for year in years:
-    url_to_scrape = f"https://en.wikipedia.org/wiki/{year}%E2%80%93{str((year + 1))[-2:]}_UEFA_Champions_League_group_stage?fbclid=IwAR1e5VgehDiy3rmX4QJAWOpNecrLKPgOIt-NCgrCEuV0zclPVTyqZh706Ro"
-    scraper = DataScraper(url_to_scrape)
-    scraper.scrape()
